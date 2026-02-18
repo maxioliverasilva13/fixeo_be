02189fb0bbb6e17478a593d6bd259e3a5da48cd0
Maximiliano Olivera <68874048+maxioliverasilva13@users.noreply.github.com>
2026-02-18 14:18:49 -0300

    fix device token creaton

--- notificaciones/serializers.py
+++ notificaciones/serializers.py
@@ -7,6 +7,16 @@ class DeviceTokenSerializer(serializers.ModelSerializer):
         model = DeviceToken
         fields = '__all__'
 
+class DeviceTokenCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeviceToken
+        fields = ['device_name', 'device_token']
+
+    def validate_device_token(self, value):
+        if DeviceToken.objects.filter(device_token=value).exists():
+            raise serializers.ValidationError("El token de dispositivo ya existe")
+        return value
+
 
 class NotificacionesSerializer(serializers.ModelSerializer):
     class Meta:
